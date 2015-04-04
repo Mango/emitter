17fe9dc639435167d0ae16be37ba5de141aa696e
Guille Paz <guille87paz@gmail.com>
2015-04-04 19:10:39 -0300

    Add browserify to package. Enable loose mode on babel.

--- babel.js
+++ babel.js
@@ -7,7 +7,7 @@ if (!fs.existsSync('./es5')) {
   fs.mkdirSync('./es5');
 }
 
-babel.transformFile('index.js', {'loose': ['es6.classes']}, function (err, result) {
+babel.transformFile('index.js', {'loose': ['es6.classes', 'es6.modules']}, function (err, result) {
   if (err) { console.log('Error : ' + err.message); }
   fs.writeFileSync('./es5/index.js', result.code);
 });
